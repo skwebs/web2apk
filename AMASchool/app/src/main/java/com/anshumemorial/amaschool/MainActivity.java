81b22b9aa7b653a0eaf0a5c4bb3c99a52faa7aea
skwebs <skwebserv@gmail.com>
2021-06-05 23:08:47 +0530

    anshumemorial.in web to apk

---
+++ AMASchool/app/src/main/java/com/anshumemorial/amaschool/MainActivity.java
@@ -0,0 +1,170 @@
+package com.anshumemorial.amaschool;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private ProgressBar progressBar;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        progressBar = findViewById(R.id.progressBar);
+
+        webView.setWebViewClient(new myWebViewClient());
+        webView.setWebChromeClient(new myWebChromeClient());
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAppCacheEnabled(true);
+
+        // Improve loading speed
+//        settings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        settings.setUseWideViewPort(true);
+//        settings.setSavePassword(true);
+        settings.setSaveFormData(true);
+//        settings.setEnableSmoothTransition(true);
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+
+//        webView.setOnKeyListener((v, keyCode, event) -> {
+//            if (keyCode== KeyEvent.KEYCODE_BACK && event.getAction() == MotionEvent.ACTION_UP && webView.canGoBack()){
+//                handler.sendEmptyMessage(1);
+//                return true;
+//            }
+//            return false;
+//        });
+        loadWebPage();
+
+    }
+
+    private void loadWebPage() {
+        if(isConnected()) {
+            webView.loadUrl("https://anshumemorial.in");
+        }else{
+//            webView.setVisibility(View.GONE);
+            progressBar.setVisibility(View.GONE);
+//            noInternetLayout.setVisibility(View.VISIBLE);
+//            swipeRefreshLayout.setRefreshing(false);
+            Toast.makeText(this, "No internet connection available | loadWebPage", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onBackPressed(){
+        if (webView.canGoBack()){
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    public boolean isConnected(){
+        //    both code group is working use any group
+
+        //     group A
+        ConnectivityManager connectivityManager = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mobile = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+        // group B
+//        ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+//        NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+//        NetworkInfo mobile = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+        return wifi.isConnected() || mobile.isConnected();
+    }
+
+    private class myWebViewClient extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (url.startsWith("mailto:")) {
+                startActivity(new Intent(Intent.ACTION_SENDTO, Uri.parse(url)));
+            } else if(url.startsWith("tel:") || url.startsWith("whatsapp:")) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+                return true;
+            }
+            return false;
+        }
+
+        @TargetApi(Build.VERSION_CODES.N)
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            final Uri uri = request.getUrl();
+            if (uri.toString().startsWith("mailto:")) {
+                //Handle mail Urls
+                startActivity(new Intent(Intent.ACTION_SENDTO, uri));
+            } else if (uri.toString().startsWith("tel:")) {
+                //Handle telephony Urls
+                startActivity(new Intent(Intent.ACTION_DIAL, uri));
+            } else if (uri.toString().startsWith("whatsapp:")) {
+                //Handle whatsapp Urls
+                startActivity(new Intent(Intent.ACTION_VIEW, uri));
+            } else {
+                //Handle Web Urls
+                view.loadUrl(uri.toString());
+            }
+            return true;
+        }
+
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            super.onReceivedError(view, errorCode, description, failingUrl);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+//            below code are not need because it handle by another place
+            progressBar.setVisibility(View.VISIBLE);
+//            swipeRefreshLayout.setRefreshing(true);
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            progressBar.setVisibility(View.GONE);
+//            swipeRefreshLayout.setRefreshing(false);
+            super.onPageFinished(view, url);
+        }
+    }
+
+    private class myWebChromeClient extends WebChromeClient {
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+//            progressBar.setVisibility(View.VISIBLE);
+//            progressBar.setProgress(newProgress);
+//
+//            if (newProgress == 100){
+//                progressBar.setVisibility(View.GONE);
+//            }
+        }
+    }
+}
